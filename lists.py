4ffa6ba895dc12287163f4e924907dbb0bb2d77f
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2022-12-23 20:07:53 +0300

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,35 @@
+if __name__ == '__main__':
+    N = int(input())
+    
+    for i in range(N):
+        prompt = input()
+        
+        
+    
+    def printList():
+        print()
+    
+    def remove():
+        # remove
+    
+    def append():
+        # append
+    
+    def insert():
+        # append
+    
+    def sortList(arr):
+        return arr.sort)
+    
+    def rev(arr):
+        return arr.sort)
+    
+    def popElement(arr):
+        return arr.sort)
+    
+     
+  
+    
+        
+    
+        
