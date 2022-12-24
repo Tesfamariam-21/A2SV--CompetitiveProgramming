1fde95d3c2e382c5b2f7e9009feaaf78574933d4
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2022-12-24 11:09:28 +0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,31 +1,49 @@
 if __name__ == '__main__':
     N = int(input())
+    li = []
+    
+    def printList():
+        print(li)
+    
+    def remove(e):
+        li.remove(e)
+    
+    def append(e):
+        li.append(e)
+    
+    def insertList(i, e):
+        li.insert(i, e)
+    
+    def sortList():
+        li.sort()
+    
+    def rev():
+        li.sort(reverse= True)
+    
+    def popElement():
+        li.pop()
+    
+
     
     for i in range(N):
         prompt = input()
         
-        
-    
-    def printList():
-        print()
-    
-    def remove():
-        # remove
-    
-    def append():
-        # append
-    
-    def insert():
-        # append
-    
-    def sortList(arr):
-        return arr.sort)
-    
-    def rev(arr):
-        return arr.sort)
-    
-    def popElement(arr):
-        return arr.sort)
+        if prompt.split()[0] == "append":
+            append(int(prompt.split()[1]))
+        elif prompt.split()[0] == "insert":
+            insertList(int(prompt.split()[1]), int(prompt.split()[2]))
+        elif prompt.split()[0] == "remove":
+            remove(int(prompt.split()[1]))
+        elif prompt.split()[0] == "sort":
+            sortList()
+        elif prompt.split()[0] == "reverse":
+            rev()
+        elif prompt.split()[0] == "pop":
+            popElement()
+        elif prompt.split()[0] == "print":
+            printList()
+        else:
+            print("incorrect input")
     
      
   
