a267830d1cbce04edb7e56427049b94460814aa2
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2022-12-30 12:39:56 +0300

    Create checkStrictSuperset.py

---
+++ checkStrictSuperset.py
@@ -0,0 +1,15 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+if __name__ == "__main__":
+    setA = set(map(int, input().split()))
+    numOfSubset = int(input())
+    strictSuperset = True
+    
+
+    
+    for i in range(numOfSubset):
+        subset = set(map(int, input().split()))
+        if not setA.issuperset(subset) or len(subset) >= len(setA):
+            strictSuperset = False
+
+    print(strictSuperset)
