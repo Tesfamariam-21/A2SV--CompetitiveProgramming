c4bd168b3fa026871d60bc75b5f6b00d396f2fe1
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2023-01-06 09:03:59 +0300

    Time: 146 ms (5.27%), Space: 13.9 MB (80.32%) - LeetHub

---
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.py
@@ -0,0 +1,48 @@
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        counter = Counter()
+        li = []
+        
+        for i in range(len(cpdomains)):
+            rep, d = cpdomains[i].split()
+            counter[d] += int(rep)
+            
+            while d.count(".") != 0:
+                index = d.index(".") + 1
+                d = d[index:len(d)]
+                counter[d] += int(rep)
+                
+        for key in counter:
+            li.append(str(counter[key]) + " " + key)
+        
+        return li
+#         from collections import defaultdict
+#         dict_ = defaultdict(int)
+#         li = []
+
+#         for i in range(len(cpdomains)):
+#             split = cpdomains[i].split()
+#             dict_[split[1]] += int(split[0])
+            
+#             while split[1].count(".") != 0:
+#                 rep = 0
+#                 index = split[1].index(".") + 1
+#                 split[1] = split[1][index:len(split[1])]
+                
+#                 for j in dict_:
+#                     if j == split[1]:
+#                         rep += dict_[j]
+#                 dict_[split[1]] = rep + int(split[0])
+            
+#         for key, value in dict_.items():
+#             li.append(str(value) + " " + key)
+        
+#         return li
+
+                
+                
+                
+                
+                
+                
+                
