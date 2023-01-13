a2fc12443065dc55f13a54232a1b2bdca05b72e2
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2023-01-13 18:01:38 +0300

    Time: 97 ms (59.11%), Space: 14 MB (8.42%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -3,12 +3,11 @@ class Solution:
         Toeplitz = False
         
         dict_ = defaultdict(set)
-        li = set()
+        
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 dict_[j-i].add(matrix[i][j])
-                li = dict_[j-i]
-                if(len(li) > 1):
+                if(len(dict_[j-i]) > 1):
                     return False
         
         return True
