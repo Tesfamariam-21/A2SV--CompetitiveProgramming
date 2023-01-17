ad822aeb5ae00008f58bd7feff0ea37a012728a6
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2023-01-17 18:16:18 +0300

    Time: 1805 ms (5.03%), Space: 14.3 MB (84.60%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -3,7 +3,7 @@ class Solution:
         
         for i in range(len(heights)):
             flag = False
-            for j in range(len(heights)-1):
+            for j in range(len(heights)-i-1):
                 if heights[j] < heights[j+1]:
                     heights[j], heights[j+1] = heights[j+1], heights[j]
                     names[j], names[j+1] = names[j+1], names[j]
