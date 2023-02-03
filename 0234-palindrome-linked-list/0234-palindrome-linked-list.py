223fba4a560600c865e6acf984ca7543b07db6a4
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2023-02-03 23:53:37 +0300

    Time: 721 ms (94.57%), Space: 38.8 MB (91.53%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,63 +5,50 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        nums = []
-        while head:
-            nums.append(head.val)
-            head = head.next
+#         o(1) space
+        fast = head
+        slow = head
+        middleIndex = 0
+        
+        # // find middle
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            middleIndex +=1
+
+#         reverse
+        prevNode = None
+        while slow:
+            nextNode = slow.next
+            slow.next = prevNode
+            prevNode = slow
+            slow = nextNode
             
-        left = 0 
-        right = len(nums) - 1
+#             check
+        left, right = head, prevNode
         
-        while left <= right:
-            if nums[left] != nums[right]:
+        while right:
+            if left.val != right.val:
                 return False
-            left +=1
-            right -=1
-        
+            left = left.next
+            right = right.next
+            
         return True
         
-#         fast = head
-#         fastIndex = 0
-#         slow = head
-#         slowIndex = 0
-#         checkNode = head
-#         prevNode = None
         
-#         while fast != None and fast.next != None:
-#             prevNode = slow
-#             slow = slow.next
-#             fast = fast.next.next
-#             fastIndex += 2
-#             slowIndex += 1
+            #     this solution works with space
+#         nums = []
+#         while head:
+#             nums.append(head.val)
+#             head = head.next
             
-          
-#         current = slow.next
-#         nextNode = slow.next.next
-#         prevNode = slow
-#         for i in range(slowIndex - fastIndex + 1):
-#             current.next = prevNode
-#             prevNode = current
-#             current = nextNode
-#             nextNode = current.next
-            
-#         current.next = prevNode
+#         left = 0 
+#         right = len(nums) - 1
         
-#         while slow != None and slow.next != None:
-            
-#             if checkNode == slow.next:
+#         while left <= right:
+#             if nums[left] != nums[right]:
 #                 return False
-            
-#             checkNode =checkNode.next
-#             slow = slow.next
-            
-            
+#             left +=1
+#             right -=1
+        
 #         return True
-        
-        
-        
-        
-        
-        
-            
-        
