733d676d3718f66b34b89002eefcfe3525a38a9e
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2023-02-23 15:54:26 +0300

    Time: 431 ms (20.10%), Space: 15.4 MB (26.13%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -2,7 +2,6 @@ from collections import OrderedDict
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         ans = []
-        # left = 0
         right = len(p)
       
         dict_ = Counter(p)
@@ -30,39 +29,3 @@ class Solution:
                 ans.append(right-len(p))
             
         return ans
-            
-#             if s[i] in window_dict:
-#                 arr = window_dict.get(s[i])
-#                 arr[0] +=1
-#                 arr[1] = i
-#                 window_dict[s[i]] = arr
-#                 index_dict[s[i]] = i
-#             else:
-#                 window_dict[s[i]] = 1
-#                 index_dict[s[i]] = i
-            
-#             if (right - left)%len(p) == 0:
-#                 for i in range(left,right):
-#                     if window_dict == dict_:
-#                         ans.append(index_dict[s])
-                        
- 
-#         for i in range(len(s)):
-#             if s[i] in window_dict:
-#                 arr = window_dict.get(s[i])
-#                 arr[0] +=1
-#                 arr[1] = i
-#                 window_dict[s[i]] = arr
-#             else:
-#                 window_dict[s[i]] = [1,i]
-            
-#             if i % len(s) == 0:
-#                 if dict_ == window_dict:
-#                     ans.append()
-#                 else:
-#                     window_dict = {}
-            
-            
-        
-        
-        
