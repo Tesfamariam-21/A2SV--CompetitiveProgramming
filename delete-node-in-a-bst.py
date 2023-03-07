f0520f5bd8b9d21ea0f160e6f3f3169c98d3d3bf
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2023-03-07 16:38:22 +0300

    added delete-node-in-a-bst.python

---
+++ delete-node-in-a-bst.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return root
+
+        if root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        elif root.val < key:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+
+            temp = self.minVal(root.right) 
+            root.val = temp.val
+
+            root.right = self.deleteNode(root.right, temp.val)
+        
+        return root
+            
+    
+    def minVal(self,root):
+        cur = root
+
+        while cur.left:
+            cur = cur.left
+        return cur
