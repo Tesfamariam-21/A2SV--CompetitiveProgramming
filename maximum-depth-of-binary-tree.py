7cdb91da004aab7db0a1e4ea3b61bdd077a7f546
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2023-03-07 14:44:35 +0300

    added maximum-depth-of-binary-tree.python

--- maximum-depth-of-binary-tree.py
+++ maximum-depth-of-binary-tree.py
@@ -6,15 +6,24 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right)) if root else 0
+        # if not root:
+        #     return 0
+            
+        # left = self.maxDepth(root.left)
+        # right = self.maxDepth(root.right)     
+        
+        # return 1 + max(left, right)
 
-        def depthTraverse(root):
-            if not root:
-                return 0
+
+# def depthTraverse(root):
+#             if not root:
+#                 return 0
             
-            left = depthTraverse(root.left)
-            right = depthTraverse(root.right)
+#             left = depthTraverse(root.left)
+#             right = depthTraverse(root.right)
             
-            return 1 + max(left, right)
+#             return 1 + max(left, right)
         
         
-        return depthTraverse(root)
+#         return depthTraverse(root)
