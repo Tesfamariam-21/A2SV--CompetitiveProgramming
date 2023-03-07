bc408e9c3fa4aeaed102f7e1ad8e926fd31e4c12
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2023-03-07 14:39:42 +0300

    added maximum-depth-of-binary-tree.python

---
+++ maximum-depth-of-binary-tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+
+        def depthTraverse(root):
+            if not root:
+                return 0
+            
+            left = depthTraverse(root.left)
+            right = depthTraverse(root.right)
+            
+            return 1 + max(left, right)
+        
+        
+        return depthTraverse(root)
