49ee1ca425f7e3af85c1dd1a58c4b6e9daf07c5b
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2023-03-08 04:07:53 +0300

    added validate-binary-search-tree.python

--- validate-binary-search-tree.py
+++ validate-binary-search-tree.py
@@ -18,3 +18,41 @@ class Solution:
             
 
         return checkValid(root, float("-inf"), float("inf"))
+
+# class Solution:
+#     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+#         answer = []
+#         maxNum = float("-inf")
+
+#         def traverse(root):
+#             nonlocal maxNum
+#             if not root:
+#                 return True
+            
+#             left = traverse(root.left)
+#             if not left:
+#                 return False
+#             if maxNum >= root.val:
+#                 return False
+#             maxNum = max(maxNum, root.val)
+#             right = traverse(root.right)
+#             if not right:
+#                 return False
+#             return True
+
+#         return traverse(root)
+
+# class Solution:
+#     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+#         arr=[]
+#         def traverse(root):
+#            if not root:
+#                return
+#            traverse(root.left)
+#            arr.append(root.val)
+#            traverse(root.right)
+#         traverse(root)
+#         for i in range(len(arr)-1):
+#             if arr[i]>=arr[i+1]:
+#                 return False
+#         return True
