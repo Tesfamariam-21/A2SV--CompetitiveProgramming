99ffca81e907a3ae7d877caec6b20bc2bc5ec221
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2023-03-08 16:47:38 +0300

    added binary-tree-paths.python

---
+++ binary-tree-paths.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        ans = []
+        way = ""
+
+        def path(root, way):
+            nonlocal ans
+            if not root:
+                return 
+ 
+            way = way + str(root.val) + "->"
+            if not root.left and not root.right:
+                ans.append(way[:-2])
+                return 
+
+            path(root.left, way)
+            path(root.right, way)         
+
+        path(root, way) 
+        
+        return ans
