f2f0e7a87667c3f57e897d26200f41d0c9bc18b4
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2023-03-10 08:04:55 +0300

    added lowest-common-ancestor-of-a-binary-search-tree.python

--- lowest-common-ancestor-of-a-binary-search-tree.py
+++ lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,48 +7,72 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        pPath = []
-        qPath = []
 
-        def pFind(root, p):
-            nonlocal pPath
-            if not root:
-                return
-            if p.val < root.val:
-                pPath.extend(pFind(root.left, p))
-                return [root]
-            elif p.val > root.val:
-                pPath.extend(pFind(root.right, p))
-                return [root]
-            else:
-                return [root]
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
+
+# class Solution:
+#     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+#         if p is None or q is None:
+#             return None
+
+#         if root==p or root==q:
+#             return root
+
+#         if (root.left==p and root.right==q) and (root.left==q and root.right==p):
+#             return root
+
+#         if p.val<root.val and q.val<root.val:
+#             return self.lowestCommonAncestor(root.left, p, q)
+
+#         if p.val>root.val and q.val>root.val:
+#             return self.lowestCommonAncestor(root.right, p, q)
+            
+#         return root
+
+        # pPath = []
+        # qPath = []
+
+        # def pFind(root, p):
+        #     nonlocal pPath
+        #     if p.val < root.val:
+        #         pPath.extend(pFind(root.left, p))
+        #         return [root.val]
+        #     elif p.val > root.val:
+        #         pPath.extend(pFind(root.right, p))
+        #         return [root.val]
+        #     else:
+        #         return [root.val]
             
        
 
-        def qFind(root, q):
-            nonlocal qPath
-            if not root:
-                return 
-            if q.val < root.val:
-                qPath.extend(qFind(root.left, q))
-                return [root]
-            elif q.val > root.val:
-                qPath.extend(qFind(root.right, q))
-                return [root]
-            else:
-                return [root]
+        # def qFind(root, q):
+        #     nonlocal qPath
+        #     if q.val < root.val:
+        #         qPath.extend(qFind(root.left, q))
+        #         return [root.val]
+        #     elif q.val > root.val:
+        #         qPath.extend(qFind(root.right, q))
+        #         return [root.val]
+        #     else:
+        #         return [root.val]
 
-        pPath.extend(pFind(root, p))
-        qPath.extend(qFind(root, q))
+        # pPath.extend(pFind(root, p))
+        # qPath.extend(qFind(root, q))
 
-        pRight = len(pPath)-1
-        qRight = len(qPath)-1
-        print(pPath, qPath)
+        # pRight = len(pPath)
+        # qRight = len(qPath)
+        # print(pPa)
 
-        while pRight >= 0 and qRight >= 0:
-            if pPath[pRight] != qPath[qRight]:
-                return pPath[pRight+1]
-            pRight -=1
-            qRight -=1
+        # while pRight >= 0 and qRight >= 0:
+        #     if pPath[pRight] != qPath[qRight]:
+        #         return pPath[pRight+1]
+        #     pRight -=1
+        #     qRight -=1
         
-        return pPath[pRight + 1]
+        # return pPath[pRight + 1]
