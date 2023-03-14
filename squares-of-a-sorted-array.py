5ee2d388d8ede51477f1620330cfe31451f0ae4b
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2023-03-14 09:21:22 +0300

    added squares-of-a-sorted-array.python

--- squares-of-a-sorted-array.py
+++ squares-of-a-sorted-array.py
@@ -1,3 +1,20 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        return sorted([x**2 for x in nums])
+        # return sorted([x**2 for x in nums])
+        # return sorted(list(map(lambda x: x**2, nums)))
+
+        left = 0
+        right = len(nums) -1
+        ans = [0 for i in range(len(nums))]
+        k = len(nums) - 1
+
+        while k >= 0:
+            if abs(nums[left]) <= abs(nums[right]):
+                ans[k] = nums[right]**2
+                right -=1
+            else:
+                ans[k] = nums[left]**2
+                left +=1
+            k -=1
+
+        return ans
