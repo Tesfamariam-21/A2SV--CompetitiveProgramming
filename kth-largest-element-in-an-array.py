d11619398a25ffea36d5d877470135826c28beed
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2023-03-23 19:30:40 +0300

    added kth-largest-element-in-an-array.python

---
+++ kth-largest-element-in-an-array.py
@@ -0,0 +1,38 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+
+        def partition(left, right):
+            if right - left <= 0:
+                if left == len(nums) - k:
+                    return nums[left]
+                return
+
+            pivot = right
+            leftmost = left
+
+            while left < right:
+                if nums[left] < nums[pivot]:
+                    left +=1
+
+                else:
+                    if nums[right] >= nums[pivot]:
+                        right -=1
+                    else:
+                        nums[left], nums[right] = nums[right], nums[left]
+                
+
+            nums[left], nums[pivot] = nums[pivot], nums[left]
+
+            if left == len(nums) - k:
+                return nums[left]
+
+            
+            l = partition(leftmost, left - 1)
+            r = partition(left + 1, pivot)
+
+            if l != None:
+                return l
+
+            return r
+
+        return partition(0, len(nums) - 1)
