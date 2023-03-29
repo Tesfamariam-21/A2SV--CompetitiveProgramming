1e77b7e6e39e8ef1fe3d8b347a3825c87932a6ac
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2023-03-29 18:16:22 +0300

    added counting-bits.python

--- counting-bits.py
+++ counting-bits.py
@@ -1,9 +1,33 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
         n = n +1 
-        ans = [0]* n
-
+        ans = []
+        
         for i in range(n):
-            ans[i] = list(bin(i)).count("1")
+            oneBits = 0
+
+            while i > 0:
+                oneBits += i&1
+                i = i >> 1
+
+            ans.append(oneBits)
 
         return ans
+        
+        # for i in range(n):
+        #     ans[i] = list(bin(i)).count("1")
+
+        # return ans
+
+
+        # def convertToBinary(i, answer):
+        #     if i == 0:
+        #         return answer
+
+        #     answer += str(i%2)
+        #     return convertToBinary(i//2, answer)
+
+        # for i in range(n):
+        #     ans[i] = convertToBinary(i, "").count("1")
+
+        # return ans
