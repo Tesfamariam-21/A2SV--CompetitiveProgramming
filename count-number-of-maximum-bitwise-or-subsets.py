ae31f1420db1d0ed4fc475f6bd68225ba38501d6
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2023-04-06 18:43:16 +0300

    added count-number-of-maximum-bitwise-or-subsets.python

--- count-number-of-maximum-bitwise-or-subsets.py
+++ count-number-of-maximum-bitwise-or-subsets.py
@@ -1,29 +1,45 @@
 class Solution:
     def countMaxOrSubsets(self, nums: List[int]) -> int:
+        interval = 0
         count = 0
         max_ = 0
+        n = len(nums)
 
-        def backtrack(i, arr):
-            nonlocal count
-            nonlocal max_
+        for i in range(n):
+            interval ^=(1<<i)
+        for i in range(0,interval+1):
             res = 0
-            for j in range(len(arr)):
-                res |=arr[j]
-
+            for j in range(20):
+                if i &(1<<j) != 0:
+                    res |= nums[j]
             if res > max_:
                 max_ = res
                 count = 1
             elif res == max_:
-                count +=1
-            
-            if len(arr) == len(nums):
-                return
+                count +=1  
 
-            for j in range(i+1, len(nums)):
-                backtrack(j, arr + [nums[j]])
-
-
-            
-
-        backtrack(-1, [])
         return count
+
+        
+        # count = 0
+        # max_ = 0
+
+        # def backtrack(i, arr):
+        #     nonlocal count
+        #     nonlocal max_
+        #     res = 0
+        #     for j in range(len(arr)):
+        #         res |=arr[j]
+
+        #     if res > max_:
+        #         max_ = res
+        #         count = 1
+        #     elif res == max_:
+        #         count +=1
+
+        #     for j in range(i+1, len(nums)):
+        #         backtrack(j, arr + [nums[j]])
+    
+
+        # backtrack(-1, [])
+        # return count
