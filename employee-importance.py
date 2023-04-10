c572fbb1821ab2e70fd2f77066e5729406d3390f
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2023-04-10 12:28:03 +0300

    added employee-importance.python

---
+++ employee-importance.py
@@ -0,0 +1,29 @@
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        emp = 0
+        importance = 0
+        dict_ = {}
+        
+        for e in employees:
+            dict_[e.id] = e
+
+        def dfs(emp):
+            nonlocal importance
+
+            importance += dict_[emp].importance
+
+            for e in dict_[emp].subordinates:
+                dfs(e)
+
+
+        dfs(id)
+        return importance
