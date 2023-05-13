ef1f2ff48e0e596e8973d92c57341ed8c2be2466
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2023-05-13 23:03:03 +0300

    added detonate-the-maximum-bombs.python

--- detonate-the-maximum-bombs.py
+++ detonate-the-maximum-bombs.py
@@ -3,27 +3,35 @@ class Solution:
         if len(bombs)==1:
             return 1
         
-        adlist={i:[] for i in range(len(bombs))}
-        
+        adjList= defaultdict(list)
+        visited = set()
+        res = 0
+       
+
         for i in range(len(bombs)):
-            x1,y1,r1=bombs[i]
-            for j in range(i+1,len(bombs)):
-                x2,y2,r2=bombs[j]
-                dist=((x2-x1)**2+(y2-y1)**2)**(1/2)
-                if dist<=r1:
-                    adlist[i].append(j)  
-                if dist<=r2:
-                    adlist[j].append(i)
+            x, y, r = bombs[i]
+            for j in range(len(bombs)):
+                if i == j:
+                    continue
+                
+                x2, y2, r2 = bombs[j]
+
+                if r**2 >= ((x- x2)**2 + (y- y2)**2):
+                    adjList[i].append(j)
+
+
+        def dfs(node, v):
+            v.add(node)
+
+            for child in adjList[node]:
+                if child not in v:
+                    dfs(child, v)
+
+            return v
+
         
-        def dfs(adlist,seen,start):
-            seen.add(start)
-            for i in adlist[start]:
-                if i not in seen:
-                    dfs(adlist,seen,i)
-        maxx=1   
-        for v in adlist:
-            seen=set()
-            seen.add(v)
-            dfs(adlist,seen,v)
-            maxx=max(maxx,len(seen))
-        return maxx
+
+        for i in range(len(bombs)):
+            res = max(res, len(dfs(i, set())))
+
+        return res
