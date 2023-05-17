fe7120bccb022558eec2110f1a3a193588031cde
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2023-05-17 18:18:41 +0300

    added find-if-path-exists-in-graph.python

--- find-if-path-exists-in-graph.py
+++ find-if-path-exists-in-graph.py
@@ -1,26 +1,58 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        adjList = defaultdict(list)
+        rep = {i:i for i in range(n)}
+        size = [1 for i in range(n)]
 
-        for e in edges:
-            adjList[e[0]].append(e[1])
-            adjList[e[1]].append(e[0])
+        def representative(member):
+            if member == rep[member]:
+                return member
+            
+            rep[member] = representative(rep[member])
+            return rep[member]
 
-        visited = set()
+        def union(member1, member2):
+            repMember1 = representative(member1)
+            repMember2 = representative(member2)
 
-        def dfs(s, visited):
-            nonlocal adjList
+            if size[repMember1] < size[repMember2]:
+                rep[repMember1] = repMember2
+                size[repMember2] += size[repMember1]
+            else:
+                rep[repMember2] = repMember1
+                size[repMember1] += size[repMember2]
 
-            if s == destination:
-                return True
+        for member1, member2 in edges:
+            union(member1, member2)
+        
 
-            visited.add(s)
+        if representative(source) == representative(destination):
+            return True
+        return False
+            
 
-            for child in adjList[s]:
-                if child not in visited:
-                    if dfs(child, visited):
-                        return True
 
-            return False
 
-        return dfs(source, visited)
+
+        # adjList = defaultdict(list)
+
+        # for e in edges:
+        #     adjList[e[0]].append(e[1])
+        #     adjList[e[1]].append(e[0])
+
+        # visited = set()
+
+        # def dfs(s, visited):
+
+        #     if s == destination:
+        #         return True
+
+        #     visited.add(s)
+
+        #     for child in adjList[s]:
+        #         if child not in visited:
+        #             if dfs(child, visited):
+        #                 return True
+
+        #     return False
+
+        # return dfs(source, visited)
