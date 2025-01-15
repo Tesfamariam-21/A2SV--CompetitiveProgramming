38413d5bd8c3d21dae3a6e737fcf94eaf784f8ed
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2025-01-15 12:22:29 +0300

    Time: 62 ms (29.54%), Space: 51.4 MB (42.55%) - LeetHub

---
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.ts
@@ -0,0 +1,22 @@
+type ToBeOrNotToBe = {
+    toBe: (val: any) => boolean;
+    notToBe: (val: any) => boolean;
+};
+
+function expect(val: any): ToBeOrNotToBe {
+    return {
+        toBe: (val2: any)=>{
+            if (val2 === val) return true
+            else throw new Error("Not Equal")
+        },
+        notToBe: (val2: any)=>{
+            if (val2 !== val) return true
+            else throw new Error("Equal")
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
