ff2759863b53b1befc56e51c726e8bc85e74f261
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2025-01-15 12:11:53 +0300

    Time: 57 ms (27.36%), Space: 48.8 MB (69.02%) - LeetHub

--- 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
@@ -3,17 +3,20 @@
  * @return {Object}
  */
 var expect = function(val) {
-    return {
-        toBe: (val2) =>{
-        if(val !== val2) throw new Error("Not Equal");
-        else return true;
-    },
-    notToBe: (val2) => {
-        if (val === val2) throw new Error("Equal");
-        else return true;
-    }
+    const toBe = (val2) =>{
+        if (val === val2) return  true
+        else throw new Error("Not Equal")
     }
     
+    const notToBe = (val2)=> {
+        if (val !== val2) return true
+        else throw new Error("Equal")
+    }
+
+    return {
+        toBe: toBe,
+        notToBe: notToBe
+    }   
 };
 
 /**
