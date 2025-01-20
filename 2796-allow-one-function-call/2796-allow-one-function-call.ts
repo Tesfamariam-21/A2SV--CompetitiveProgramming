74522c4ae25f806592b8caa657476e390dd12d2a
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2025-01-20 08:56:08 +0300

    Time: 58 ms (55.87%), Space: 51.7 MB (8.66%) - LeetHub

---
+++ 2796-allow-one-function-call/2796-allow-one-function-call.ts
@@ -0,0 +1,22 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    let calls = 0
+    
+    return function (...args) {
+        if (calls) return undefined
+
+        calls = 1
+        return fn.apply(this, args)
+
+    };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
