bd7bc7ff22b0e6c806711d306fb04d890a8445ff
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2025-01-22 10:16:21 +0300

    Time: 315 ms (29.73%), Space: 87.8 MB (63.89%) - LeetHub

---
+++ 2731-memoize/2731-memoize.js
@@ -0,0 +1,35 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const memo = {}
+
+    return function(...args) {
+        key = JSON.stringify(args)
+
+        if (key in memo){
+            return memo[key]
+        }
+
+        // if(memo[key]) return memo[key]
+        // Purpose: This checks whether the value of memo[key] is truthy. 
+        //In JavaScript, values like 0, null, undefined, NaN, false, and empty strings ('') 
+        //are considered falsy.
+
+        memo[key] = fn(...args)        
+        return memo[key] 
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
