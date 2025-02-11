3d8f3474ef12e11ddeae8fe57084707a86a5b55e
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2025-02-11 09:12:41 +0300

    Time: 13 ms (21.55%), Space: 19 MB (8.97%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,16 +1,50 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        left = 0
-        right = 0
-        while right < len(nums):
-            if nums[right] != 0:
-                nums[left] = nums[right]
-                right += 1
-                left += 1
+        ptr1, ptr2 = 0, 0
+        
+        while ptr2 < len(nums):
+            if nums[ptr1] != 0:
+                ptr1 +=1 
+                ptr2 +=1
+            elif nums[ptr2] == 0:
+                ptr2 +=1
             else:
-                right+=1
-        for i in range(left, len(nums)):
-            nums[i] = 0
-        return nums
+                nums[ptr1], nums[ptr2] = nums[ptr2], nums[ptr1]
+        
+        # ptr = 0
 
-                
+        # for i in range(len(nums)):
+        #     if nums[i]:
+        #         nums[ptr] = nums[i]
+        #         ptr +=1
+        
+        # nums[ptr:] = [0] * (len(nums) - ptr)
+
+        # left = 0
+
+        # for i in range(len(nums)):
+        #     if nums[i]:
+        #         nums[i], nums[left] = nums[left], nums[i]
+        #         left +=1
+
+
+
+
+        # read = 0
+        # write = 0
+
+        # while read < len(nums):
+        #     if nums[write] != 0:
+        #         read +=1
+        #         write +=1
+        #     elif nums[read] != 0:
+        #         nums[write], nums[read] = nums[read], nums[write]
+        #     else:
+        #         read +=1
+
+        
+        # # for i in range(len(nums)):
+        # #     if nums[i] == 0:
+        # #         nums.append(nums.pop(i))
+
+        # # return nums
