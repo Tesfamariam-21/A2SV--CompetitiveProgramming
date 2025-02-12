d73dc04826636075ba38146e7bb6509ebfa0d2e1
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2025-02-12 15:02:58 +0300

    Time: 0 ms (100%), Space: 17.9 MB (28.24%) - LeetHub

--- 0777-toeplitz-matrix/0777-toeplitz-matrix.py
+++ 0777-toeplitz-matrix/0777-toeplitz-matrix.py
@@ -1,26 +1,26 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        
         rows, cols = len(matrix), len(matrix[0])
 
-        dict_ = defaultdict(set)
-
-        for i in range(rows):
-            for j in range(cols):
-                key = i - j
-                dict_[key].add(matrix[i][j])
-                if len(dict_[key]) > 1:
+        for i in range(rows - 1):
+            for j in range(cols - 1):
+                if matrix[i][j] != matrix[i + 1][j + 1]:
                     return False
-                
-        
         return True
 
         # rows, cols = len(matrix), len(matrix[0])
 
-        # for i in range(rows - 1):
-        #     for j in range(cols - 1):
-        #         print(i - j)
-        #         if matrix[i][j] != matrix[i + 1][j + 1]:
+        # dict_ = defaultdict(set)
+
+        # for i in range(rows):
+        #     for j in range(cols):
+        #         key = i - j
+        #         dict_[key].add(matrix[i][j])
+        #         if len(dict_[key]) > 1:
         #             return False
+                
+        
         # return True
 
                 
