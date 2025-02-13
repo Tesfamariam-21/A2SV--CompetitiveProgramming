6ece7a82945a1c6c524beac38b5b8306eb0b8bbe
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2025-02-13 14:14:40 +0300

    Time: 6 ms (45.11%), Space: 17.7 MB (67.16%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,26 +1,47 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row = defaultdict(set)
-        col = defaultdict(set)
-        square = defaultdict(set)
-        
+    
         for i in range(9):
+            set_ = set()
             for j in range(9):
-                if board[i][j] == ".":
-                    continue
-                if board[i][j] in row[i] or board[i][j] in col[j] or board[i][j] in square[(i//3, j//3)]:
-                    return False
-                
-                row[i].add(board[i][j])
-                col[j].add(board[i][j])
+                if board[i][j] != ".":
+                    if board[i][j] in set_:
+                        return False
+                    set_.add(board[i][j])
+                         
+
+        for i in range(9):
+            set_ = set()
+            for j in range(9):
+                if board[j][i] != ".":
+                    if board[j][i] in set_:
+                        return False
+                    set_.add(board[j][i])
                     
-        for i in range(9):
-            for j in range(9):
-                if board[i][j] == ".":
-                    continue
-                if board[i][j] in square[(i//3, j//3)]:
-                    return False
-                square[(i//3, j//3)].add(board[i][j])
+        
+        for k in range(0, 9, 3):
+            for l in range(0, 9, 3):
+                set_ = set()
+                for i in range(k, k + 3):
+                    for j in range(l, l + 3):
+                        if board[i][j] != ".":
+                            if board[i][j] in set_:
+                                return False
+                        set_.add(board[i][j]) 
                 
         return True
-        
+
+
+        # ans = []
+
+        # for i in range(9):
+        #     for j in range(9):
+        #         if board[i][j] == ".":
+        #             continue
+
+        #         item = board[i][j]
+        #         ans += [(i, item), (item, j), (i//3, j//3, item)]
+
+        # return len(ans) == len(set(ans))
+
+
