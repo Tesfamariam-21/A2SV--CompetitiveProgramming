ce7391357ab3bbfde62990bf117237c28c03015e
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2025-06-02 09:19:35 +0300

    Time: 256 ms (9.26%), Space: 59.7 MB (28.91%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,13 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        ans = []
+        
+        def backtrack(j, arr):
+            if len(arr) == k:
+                ans.append(arr)
+            
+            for i in range(j+1, n+1):
+                backtrack(i, arr + [i])
+
+        backtrack(0, [])
+        return ans
