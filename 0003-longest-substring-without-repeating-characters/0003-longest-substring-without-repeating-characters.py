1602649e7e351d07d03bd9726679757cbc5b26fc
Tesfamariam Amsalu <88876778+Tesfamariam-21@users.noreply.github.com>
2025-06-25 09:28:35 +0300

    Time: 15 ms (81.86%), Space: 17.8 MB (52.82%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,32 +1,75 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        left = 0
-        right = 0
-        dict_ = {}
-        max_ = 0
-        
-        while right < len(s):
-            if s[right] in dict_:
-                left = max(left,dict_[s[right]]+1)
-                
 
+        right = left = 0
+        dict_ = {}
+        length = len(s)
+        longest_substring = 0
+
+        while right < length:
+            if s[right] in dict_:
+                left = max(left, dict_[s[right]] + 1)
             
             dict_[s[right]] = right
-            max_ = max(max_, right - left + 1)
             right +=1
-        
-        return max_
-        
-        
-#         left = 0
-#         dict_ = {}
-#         maxLength = 0
-        
-#         for right in range(len(s)):
-#             if s[right] in dict_:
-#                 left =max(left, dict_[s[right]] + 1)
+            longest_substring = max(longest_substring, right - left)
 
-#             maxLength = max(maxLength, (right - left + 1))
-#             dict_[s[right]] = right
-         
-#         return maxLength
+        return longest_substring
+
+
+        # longestSubstring = 0
+        # length = len(s)
+        # substring = set()
+        # j = 0
+
+        # for i in range(length):
+
+        #     while s[i] in substring:
+        #         substring.remove(s[j])
+        #         j +=1
+
+
+        #     substring.add(s[i])
+        #     longestSubstring = max(longestSubstring, i - j + 1)
+           
+
+        # return longestSubstring
+
+
+
+
+
+
+
+
+
+
+        
+        # dict_ = {}
+        # left = right = longest_substring= 0
+
+        # while right < len(s):
+        #     if s[right] in dict_:
+        #         left = max(left, dict_[s[right]] + 1)
+
+        #     dict_[s[right]] = right
+        #     right +=1
+        #     longest_substring = max(longest_substring, right - left)
+
+        # return longest_substring
+
+        # longest_substring = 0
+        # left = right = 0
+        # substrings = set()
+
+        # while right < len(s):
+        #     if s[right] in substrings:
+        #         substrings.discard(s[left])
+        #         left +=1
+        #     else:
+        #         substrings.add(s[right])
+        #         right +=1
+        #         longest_substring = max(longest_substring, right - left)
+
+        # return longest_substring
+
